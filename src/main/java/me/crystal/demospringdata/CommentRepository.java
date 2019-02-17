218f6dbb88e3a05ebff3072019247319bc42f386
crystal <crystal2840@gmail.com>
2019-02-17 18:32:07 +0900

    :fire: 비동기쿼리 (비추천)

--- src/main/java/me/crystal/demospringdata/CommentRepository.java
+++ src/main/java/me/crystal/demospringdata/CommentRepository.java
@@ -1,12 +1,15 @@
 package me.crystal.demospringdata;
 
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.util.concurrent.ListenableFuture;
 
 import java.util.List;
+import java.util.concurrent.Future;
 
 public interface CommentRepository extends MyRepository<Comment, Long> {
 
-    List<Comment> findByCommentContainsIgnoreCaseAndLikeCountGreaterThan(String keyword, int likeCount);
+    @Async
+    ListenableFuture<List<Comment> > findByCommentContainsIgnoreCase(String keyword, Pageable pageable);
 
 }
--- src/test/java/me/crystal/demospringdata/CommentRepositoryTest.java
+++ src/test/java/me/crystal/demospringdata/CommentRepositoryTest.java
@@ -5,10 +5,18 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.util.concurrent.ListenableFuture;
+import org.springframework.util.concurrent.ListenableFutureCallback;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -21,15 +29,37 @@ public class CommentRepositoryTest {
 
 
     @Test
-    public void crud() {
-        Comment comment = new Comment();
-        comment.setLikeCount(100);
-        comment.setComment("spring data jpa");
+    public void crud() throws ExecutionException, InterruptedException {
+        this.createComment(100, "spring data jpa");
+        this.createComment(55, "hibernate SPRING");
 
-        commentRepository.save(comment);
+        PageRequest pageRequest = PageRequest.of(0, 10, Sort.by(Sort.Direction.DESC, "LikeCount"));
 
-        List<Comment> comments = commentRepository.findByCommentContainsIgnoreCaseAndLikeCountGreaterThan("Spring",10);
-        assertThat(comments.size()).isEqualTo(1);
+        Future<List<Comment>> future = commentRepository.findByCommentContainsIgnoreCase("Spring", pageRequest);
+        System.out.println("==========");
+        System.out.println("is done?"+future.isDone());
+
+        ((ListenableFuture<List<Comment>>) future).addCallback(new ListenableFutureCallback<List<Comment>>(){
+
+            @Override
+            public void onFailure(Throwable throwable) {
+                System.out.println(throwable);
+            }
+
+            @Override
+            public void onSuccess(List<Comment> comments) {
+                System.out.println("==============");
+                comments.forEach(System.out::println);
+
+            }
+        });
+    }
+
+    private void createComment(int likeCount, String comment) {
+        Comment newComment = new Comment();
+        newComment.setLikeCount(likeCount);
+        newComment.setComment(comment);
+        commentRepository.save(newComment);
     }
 
 }
